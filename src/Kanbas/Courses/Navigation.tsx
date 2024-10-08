f3dd099da09a1667afc4c1b2cf1c34bcd0d62ce7
Tyler Gilman <tygilman@veliolabs.com>
2024-10-08 16:22:11 -0400

    navigation for courses js

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,21 +1,25 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "../styles.css"; 
 export default function CoursesNavigation() {
+
+  const { pathname } = useLocation();
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
       <Link to="/Kanbas/Courses/1234/Home" id="wd-course-home-link"
-        className="list-group-item active border border-0"> Home </Link><br />
+        className={`list-group-item border border-0 ${pathname.includes("Home") ? "active" : "text-danger"}`}> Home </Link><br />
       <Link to="/Kanbas/Courses/1234/Modules" id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"> Modules </Link><br />
+        className={`list-group-item text-danger border border-0 ${pathname.includes("Modules") ? "active text-black" : "text-danger"}`}> Modules </Link><br />
       <Link to="/Kanbas/Courses/1234/Piazza" id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"> Piazza </Link><br />
+        className={`list-group-item border border-0 ${pathname.includes("Piazza") ? "active text-black" : "text-danger"}`}> Piazza </Link><br />
       <Link to="/Kanbas/Courses/1234/Zoom" id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"> Zoom </Link><br />
+        className={`list-group-item text-danger border border-0 ${pathname.includes("Zoom") ? "active text-black" : "text-danger"}`}> Zoom </Link><br />
       <Link to="/Kanbas/Courses/1234/Assignments" id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"> Assignments </Link><br />
+        className={`list-group-item text-danger border border-0 ${pathname.includes("Assignments") ? "active text-black" : "text-danger"}`}> Assignments </Link><br />
       <Link to="/Kanbas/Courses/1234/Quizzes" id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"> Quizzes </Link><br />
+        className={`list-group-item text-danger border border-0 ${pathname.includes("Quizzes") ? "active text-black" : "text-danger"}`}> Quizzes </Link><br />
       <Link to="/Kanbas/Courses/1234/People" id="wd-course-people-link"
-        className="list-group-item text-danger border border-0" > People </Link><br />
+        className={`list-group-item text-danger border border-0 ${pathname.includes("People") ? "active text-black" : "text-danger"}`}> People </Link><br />
     </div>
 );}
+
+
