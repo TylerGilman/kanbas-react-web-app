c011492207dde2c68dfa2f65e790d8a7a1b31eaf
Tyler Gilman <tygilman@veliolabs.com>
2024-10-31 14:50:01 -0400

    2.0-2.4

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -3,10 +3,12 @@ import { Route, Routes, Navigate } from "react-router";
 import TOC from "./TOC";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
+import Lab4 from "./Lab4";
 export default function Labs() {
   return (
     <div>
       <h1>Tyler Gilman</h1>
+      <p> Section 2 </p>
       <h2>Labs</h2>
       <TOC />
       <Routes>
@@ -14,6 +16,7 @@ export default function Labs() {
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3/*" element={<Lab3 />} />
+        <Route path="Lab4" element={<Lab4 />} />
       </Routes>
     </div>
   );
