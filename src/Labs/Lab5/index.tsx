0aa67bafe20e40319ed577fcb632d8fea6393c63
Tyler Gilman <tylersgilman@gmail.com>
2024-11-18 19:24:06 -0500

    3.5.6

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -3,6 +3,9 @@ import PathParameters from "./PathParameters";
 import QueryParameters from "./QueryParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithArrays from "./WorkingWithArrays";
+import HttpClient from "./HttpClient";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 const REMOTE_SERVER = process.env.REACT_APP_REMOTE_SERVER;
 export default function Lab5() {
   return (
@@ -23,6 +26,12 @@ export default function Lab5() {
       <WorkingWithObjects/>
       <hr/>
       <WorkingWithArrays/>
+      <hr/>
+      <HttpClient/>
+      <hr/>
+      <WorkingWithObjectsAsynchronously/>
+      <hr/>
+      <WorkingWithArraysAsynchronously/>
     </div>
   );
 }
