af542b4039cf0f2e21e706b2719fb4f4c83b2ff4
Tyler Gilman <tylersgilman@gmail.com>
2024-11-21 12:26:42 -0500

    4.5

--- src/Kanbas/Courses/Modules/index.tsx
+++ src/Kanbas/Courses/Modules/index.tsx
@@ -33,6 +33,10 @@ export default function Modules() {
     await modulesClient.deleteModule(moduleId);
     dispatch(deleteModule(moduleId));
   };
+  const saveModule = async (module: any) => {
+    await modulesClient.updateModule(module);
+    dispatch(updateModule(module));
+  };
 
   return (
     <div className="wd-modules container">
@@ -60,7 +64,7 @@ export default function Modules() {
                     }
                     onKeyDown={(e) => {
                       if (e.key === "Enter") {
-                        dispatch(updateModule({ ...module, editing: false }));
+                     saveModule({ ...module, editing: false });
                       }
                     }}
                     defaultValue={module.name} 
