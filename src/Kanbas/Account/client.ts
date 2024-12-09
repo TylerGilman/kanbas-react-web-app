d738555943b5c0359b6009b87911b704929ca476
Tyler Gilman <tylersgilman@gmail.com>
2024-12-09 02:46:02 -0500

    3.6.3

--- src/Kanbas/Account/client.ts
+++ src/Kanbas/Account/client.ts
@@ -41,3 +41,13 @@ export const createCourse = async (course: any) => {
   );
   return response.data;
 };
+export const findUsersByRole = async (role: string) => {
+  const response = await
+    axios.get(`${USERS_API}?role=${role}`);
+  return response.data;
+};
+
+export const findUsersByPartialName = async (name: string) => {
+  const response = await axios.get(`${USERS_API}?name=${name}`);
+  return response.data;
+};
